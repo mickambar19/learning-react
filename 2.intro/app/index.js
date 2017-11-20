ff0a9b247ddec0c7b0216fbe9ef81cde40051911
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2017-11-20 12:29:31 -0600

    Changed to badge component

--- 2.intro/app/index.js
+++ 2.intro/app/index.js
@@ -3,26 +3,29 @@ var ReactDOM = require('react-dom');
 
 require('./index.css');
 
-class App extends React.Component{
+class Badge extends React.Component{
   render(){
     var name='Alexis Miguel';
     var friends=['Luigi', 'Raul', 'Diego'];
     return (
       <div>
-        {this.props.name}
-        <ul>
-          {
-            friends.map(function(friend){
-              return <li>{friend}</li>
-            })
-          }
-        </ul>
+        <img
+          src={this.props.img}
+          alt="Avatar"
+          style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>username: {this.props.username}</h3>
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <App name="The name"/>,
+  <Badge
+    name='Alexis Jimenez'
+    username='mickambar19'
+    img='https://avatars3.githubusercontent.com/u/9437615?s=460&v=4'
+  />,
   document.getElementById('app')
 );
