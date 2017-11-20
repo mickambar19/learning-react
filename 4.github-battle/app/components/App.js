263c28aa783508d7cafd17cc37d98523d373cd6b
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2017-11-20 15:35:14 -0600

    Popular component created

--- 4.github-battle/app/components/App.js
+++ 4.github-battle/app/components/App.js
@@ -1,10 +1,11 @@
 var React = require('react');
-
+var Popular = require('./Popular');
 class App extends React.Component{
     render() {
         return (
             <div>
                Hello world
+              <Popular/>
             </div>
         )
     }
