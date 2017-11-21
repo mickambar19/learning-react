916164e87fa1402175ddda97d88895d29a529505
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2017-11-20 23:41:21 -0600

    Abstracting SelectLanguage Stateless Component

--- 4.github-battle/app/components/Popular.js
+++ 4.github-battle/app/components/Popular.js
@@ -1,4 +1,29 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+class SelectLanguage extends React.Component{
+    render() {
+      var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+      return (
+        <ul className="languages">
+          {
+            languages.map(function (lan) {
+              return (
+                <li
+                  style={lan === this.props.selectedLanguage ? {color: 'red'}: null }
+                  onClick={this.props.onSelectLanguage.bind(null, lan)}
+                  key={lan}>{lan}</li>
+              )
+            }, this)
+          }
+        </ul>
+      )
+    }
+}
+
+SelectLanguage.propType={
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelectLanguage: PropTypes.func.isRequired
+};
 
 class Popular extends React.Component{
   constructor(props){
@@ -18,20 +43,14 @@ class Popular extends React.Component{
   }
 
   render() {
-      var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+
       return (
-        <ul className="languages">
-          {
-            languages.map(function (lan) {
-              return (
-                <li
-                  style={lan === this.state.selectedLanguage ? {color: 'red'}: null }
-                  onClick={this.updateLanguage.bind(null, lan)}
-                  key={lan}>{lan}</li>
-              )
-            }, this)
-          }
-        </ul>
+        <div>
+          <SelectLanguage
+            onSelectLanguage={this.updateLanguage}
+            selectedLanguage={this.state.selectedLanguage}
+          />
+        </div>
       )
   }
 }
