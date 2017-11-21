749b703dc315226516f385133208699c1dfb6210
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2017-11-20 18:54:39 -0600

    Added props to constructor

--- 4.github-battle/app/components/App.js
+++ 4.github-battle/app/components/App.js
@@ -3,8 +3,7 @@ var Popular = require('./Popular');
 class App extends React.Component{
     render() {
         return (
-            <div>
-               Hello world
+            <div className="container">
               <Popular/>
             </div>
         )
--- 4.github-battle/app/components/Popular.js
+++ 4.github-battle/app/components/Popular.js
@@ -1,13 +1,39 @@
 var React = require('react');
 
 class Popular extends React.Component{
-    render() {
-        return (
-            <div>
-              Popular
-            </div>
-        )
-    }
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang){
+    this.setState(function() {
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
+
+  render() {
+      var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+      return (
+        <ul className="languages">
+          {
+            languages.map(function (lan) {
+              return (
+                <li
+                  style={lan === this.state.selectedLanguage ? {color: 'red'}: null }
+                  onClick={this.updateLanguage.bind(null, lan)}
+                  key={lan}>{lan}</li>
+              )
+            }, this)
+          }
+        </ul>
+      )
+  }
 }
 
 module.exports = Popular;
